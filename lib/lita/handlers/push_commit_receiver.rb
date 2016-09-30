879241604d03f1e9f44165be8a263552c5949f90
Mario López <mariolopezlandes@gmail.com>
2016-09-30 17:09:27 -0300

    feat(github): github hooks codebase

---
+++ lib/lita/handlers/push_commit_receiver.rb
@@ -0,0 +1,21 @@
+require 'lita-github-web-hooks-core'
+
+module Lita
+  module Handlers
+    class GithubPushReceiver < Lita::Extensions::GitHubWebHooksCore::HookReceiver
+      def self.name
+        "GithubPushReceiver"
+      end
+
+      http.post "/github-web-hooks", :receive_hook
+
+      on :push, :process_push
+
+      def process_push
+      	
+      end
+
+      Lita.register_handler(self)
+    end
+  end
+end
