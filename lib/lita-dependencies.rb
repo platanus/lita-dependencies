61d773d38d27b5c64c3351e0a1fca9ce6435b466
Mario López <mario@platan.us>
2016-10-07 15:19:33 -0300

    feat(web hook): receiving ping from github

--- lib/lita-dependencies.rb
+++ lib/lita-dependencies.rb
@@ -4,9 +4,8 @@ Lita.load_locales Dir[File.expand_path(
   File.join("..", "..", "locales", "*.yml"), __FILE__
 )]
 
-require "lita/handlers/dependencies"
+require "lita/handlers/push_commit_receiver"
 
-Lita::Handlers::Dependencies.template_root File.expand_path(
-  File.join("..", "..", "templates"),
- __FILE__
+Lita::Handlers::GithubPushReceiver.template_root File.expand_path(
+  File.join("..", "..", "templates"), __FILE__
 )
--- lib/lita/handlers/push_commit_receiver.rb
+++ lib/lita/handlers/push_commit_receiver.rb
@@ -1,4 +1,4 @@
-require 'lita-github-web-hooks-core'
+require "lita-github-web-hooks-core"
 
 module Lita
   module Handlers
@@ -9,11 +9,19 @@ module Lita
 
       http.post "/github-web-hooks", :receive_hook
 
-      on :push, :process_push
-      
+      on :push, :store
+      on :ping, :just_ping
 
-      def process_push
-        puts "Commit detected"
+      def logger
+        Lita.logger
+      end
+
+      def store(_payload)
+        logger.debug("Payload received")
+      end
+
+      def just_ping(_payload)
+        logger.debug("Ping received from Github with success!")
       end
 
       Lita.register_handler(self)
