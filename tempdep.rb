dd58d3cc5e9ed3b7b7ebb8e162ad19b3875d569c
Felipe Balart <felbalart@gmail.com>
2016-10-28 12:39:15 -0300

    wip

---
+++ tempdep.rb
@@ -0,0 +1,3 @@
+require "power-types"
+require "/Users/felbalart/src/lita-dependencies/lib/lita/commands/process_entry.rb"
+require "/Users/felbalart/src/lita-dependencies/lib/lita/models/gem_entry.rb"
