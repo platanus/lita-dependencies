058842196ab21aca5caa543a6695f192a7296269
Felipe Balart <felbalart@gmail.com>
2016-11-09 01:11:13 -0300

    fix(slack-message): fixes indentation and line breaks

--- lib/lita/commands/build_message.rb
+++ lib/lita/commands/build_message.rb
@@ -3,9 +3,9 @@ class BuildMessage < PowerTypes::Command.new(:entry, :previous_entries)
     use_count = @previous_entries.count + 1
 
     this_entry_msg = %Q(
-    #{@entry.user} incluyó la gema '#{@entry.gem_name}' en el proyecto #{@entry.project}.
-    Es la #{use_count}º vez que se usa en Platanus.
-  )
+#{@entry.user} incluyó la gema '#{@entry.gem_name}' en el proyecto #{@entry.project}.
+Es la #{use_count}º vez que se usa en Platanus.
+)
     use_history_msg =
         case use_count
           when 0..1
@@ -18,7 +18,7 @@ class BuildMessage < PowerTypes::Command.new(:entry, :previous_entries)
             "Las últimas 2 veces fueron #{entry_phrase(@previous_entries[0])}, y #{entry_phrase(@previous_entries[1])}"
         end
 
-    this_entry_msg + use_history_msg
+    this_entry_msg + use_history_msg + "\n"
   end
 
   def entry_phrase(_entry)
